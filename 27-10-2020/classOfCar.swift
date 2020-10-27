c2957a000e892e8cbc675bfd4109c2e22ee6d714
Anusree <anusree.kb@robosoftin.com>
2020-10-27 16:12:06 +0530

    assignments on classes

---
+++ 27-10-2020/classOfCar.swift
@@ -0,0 +1,36 @@
+import Foundation
+ class car {
+     var company: String
+     var model: String
+     var seatCapacity: Int
+     var fuelType: String
+     var price: Int
+
+ init(company: String, model: String, seatCapacity: Int, fuelType: String, price: Int) {
+
+      self.company = company
+      self.model = model
+      self.seatCapacity = seatCapacity
+      self.fuelType = fuelType
+      self.price = price
+ }
+  
+func isAffortable(price: Int) -> Bool {
+
+    if (price < 1000000){
+        return true
+
+    }
+    return false
+}
+
+func requireFiveSeat(seat: Int) -> Bool {
+
+    if (seat == 5){
+        return true
+    }
+    return false
+}
+}
+
+ let car1 = car (company: "ABC", model: "AAA", seatCapacity: 5, fuelType: "petrol", price: 600000)
---
+++ 27-10-2020/classOfRestaurantMenu.swift
@@ -0,0 +1,35 @@
+import Foundation
+ class restaurantMenu {
+     var category: String
+     var type: String
+     let name: String
+     var quantity: Int
+     let price: Int
+
+ init(category: String, type: String, name: String, quantity: Int, price: Int) {
+      self.category = category
+      self.name = name
+      self.type = type
+      self.quantity = quantity
+      self.price = price
+ }
+  
+func isAffortable(price: Int) -> Bool {
+
+    if (price < 500) {
+        return true
+
+    }
+    return false
+}
+
+func isSpicy(item: String) -> Bool {
+
+    if (item == "spicy") {
+        return true
+    }
+    return false
+}
+}
+
+ let menu = restaurantMenu (category: "starters", type: "spicy", name: "ABC", quantity: 2, price: 150)
